e6b2915b144e4d99e44be6c60eb0825f0965f7cb
ZeroDiego <82512083+ZeroDiego@users.noreply.github.com>
2022-05-07 18:22:33 +0200

    DiegoScene Updatering

--- Veckoredovisningen/Assets/Scripts/PlayerController.cs
+++ Veckoredovisningen/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
 	{
 		input = new PlayerControlInput();
         switchTrigger = GameObject.Find("Switch").GetComponent<SwitchTrigger>();
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("White"), LayerMask.NameToLayer("Black"));
     }
 
 	private void OnEnable()
